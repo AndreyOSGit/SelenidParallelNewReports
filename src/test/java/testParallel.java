8d2463990ab8be20d4b375991aa23440ce56d534
AndreyOSGit <osetsky.ay@gmail.com>
2018-04-18 02:37:05 +0300

    paralleld by groups

--- src/test/java/testParallel.java
+++ src/test/java/testParallel.java
@@ -6,8 +6,11 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import javax.xml.soap.Text;
+
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
+
 
 public class testParallel {
 
@@ -22,7 +25,7 @@ public class testParallel {
 //            return new FirefoxDriver(capabilities);
 //        }
 
-    @BeforeClass
+    @BeforeClass (groups = {"google","wikipedia","youtube"})
     public void creatTest()
     {
         // whant my driver https://github.com/codeborne/selenide/wiki/How-Selenide-creates-WebDriver
@@ -33,7 +36,7 @@ public class testParallel {
         System.out.println("Before testParallel-method. Thread id is: " + id);
 
     }
-    @Test //(groups = {"smoke","id1"})
+    @Test (groups = {"google"})
     public void testM()
     {
         long id = Thread.currentThread().getId();
@@ -45,10 +48,10 @@ public class testParallel {
         $("#lst-ib").pressEnter();
         $("#hplogo").should(disappear); // Waits until element disappears
         $("#resultStats").shouldHave(text("Результатов"));
-
+        System.out.println("G1");
         WebDriverRunner.closeWebDriver();
     }
-    @Test//(groups = {"smoke","id2"})
+    @Test (groups = {"google"})
     public void testM1()
     {
         long id = Thread.currentThread().getId();
@@ -59,10 +62,11 @@ public class testParallel {
         $("#lst-ib").setValue("bruse connor");
         $("#lst-ib").pressEnter();
         $("#hplogo").should(disappear); // Waits until element disappears
-        $("#resultStats").shouldHave(text("Результатов"));
+        $("#resultStats").shouldHave(text("Резульатов"));
         WebDriverRunner.closeWebDriver();
+        System.out.println("G2");
     }
-    @Test//(groups = {"smoke","id3"})
+    @Test (groups = {"google"})
     public void testM2()
     {
         long id = Thread.currentThread().getId();
@@ -75,9 +79,99 @@ public class testParallel {
         $("#hplogo").should(disappear); // Waits until element disappears
         $("#resultStats").shouldHave(text("Результатов"));
         WebDriverRunner.closeWebDriver();
+        System.out.println("G3");
     }
-
-    @AfterClass
+    @Test (groups = {"wikipedia"})
+    public void testM3()
+    {
+        long id = Thread.currentThread().getId();
+        System.out.println("Simple testParallel-method Three. Thread id is: " + id);
+        WebDriver myWebDriver = new ChromeDriver();
+        WebDriverRunner.setWebDriver(myWebDriver); // .setWebDriver(myWebDriver).
+        open("http://google.com");
+        $("#lst-ib").setValue("news1");
+        $("#lst-ib").pressEnter();
+        $("#hplogo").should(disappear); // Waits until element disappears
+        $("#resultStats").shouldHave(text("Результатов"));
+        WebDriverRunner.closeWebDriver();
+        System.out.println("W1");
+    }
+    @Test (groups = {"wikipedia"})
+    public void testM4()
+    {
+        long id = Thread.currentThread().getId();
+        System.out.println("Simple testParallel-method Three. Thread id is: " + id);
+        WebDriver myWebDriver = new ChromeDriver();
+        WebDriverRunner.setWebDriver(myWebDriver); // .setWebDriver(myWebDriver).
+        open("http://google.com");
+        $("#lst-ib").setValue("news2");
+        $("#lst-ib").pressEnter();
+        $("#hplogo").should(disappear); // Waits until element disappears
+        $("#resultStats").shouldHave(text("Результатов"));
+        WebDriverRunner.closeWebDriver();
+        System.out.println("W2");
+    }
+    @Test (groups = {"wikipedia"})
+    public void testM5()
+    {
+        long id = Thread.currentThread().getId();
+        System.out.println("Simple testParallel-method Three. Thread id is: " + id);
+        WebDriver myWebDriver = new ChromeDriver();
+        WebDriverRunner.setWebDriver(myWebDriver); // .setWebDriver(myWebDriver).
+        open("http://google.com");
+        $("#lst-ib").setValue("news3");
+        $("#lst-ib").pressEnter();
+        $("#hplogo").should(disappear); // Waits until element disappears
+        $("#resultStats").shouldHave(text("Резульатов"));
+        WebDriverRunner.closeWebDriver();
+        System.out.println("W3");
+    }
+    @Test (groups = {"youtube"})
+    public void testM6()
+    {
+        long id = Thread.currentThread().getId();
+        System.out.println("Simple testParallel-method Three. Thread id is: " + id);
+        WebDriver myWebDriver = new ChromeDriver();
+        WebDriverRunner.setWebDriver(myWebDriver); // .setWebDriver(myWebDriver).
+        open("https://www.youtube.com");
+        $( "#search").setValue("114");
+        $("#lst-ib").pressEnter();
+        $("#hplogo").should(disappear); // Waits until element disappears
+        $("#resultStats").shouldHave(text("Резульатов"));
+        WebDriverRunner.closeWebDriver();
+        System.out.println("Y1");
+    }
+    @Test (groups = {"youtube"})
+    public void testM7()
+    {
+        long id = Thread.currentThread().getId();
+        System.out.println("Simple testParallel-method Three. Thread id is: " + id);
+        WebDriver myWebDriver = new ChromeDriver();
+        WebDriverRunner.setWebDriver(myWebDriver); // .setWebDriver(myWebDriver).
+        open("https://www.youtube.com");
+        $( "#search").setValue("114");
+        $("#lst-ib").pressEnter();
+        $("#hplogo").should(disappear); // Waits until element disappears
+        $("#resultStats").shouldHave(text("Резульатов"));
+        WebDriverRunner.closeWebDriver();
+        System.out.println("Y2");
+    }
+    @Test (groups = {"youtube"})
+    public void testM8()
+    {
+        long id = Thread.currentThread().getId();
+        System.out.println("Simple testParallel-method Three. Thread id is: " + id);
+        WebDriver myWebDriver = new ChromeDriver();
+        WebDriverRunner.setWebDriver(myWebDriver); // .setWebDriver(myWebDriver).
+        open("https://www.youtube.com");
+        $( "#search").setValue("114");
+        $("#lst-ib").pressEnter();
+        $("#hplogo").should(disappear); // Waits until element disappears
+        $("#resultStats").shouldHave(text("Резульатов"));
+        WebDriverRunner.closeWebDriver();
+        System.out.println("Y3");
+    }
+    @AfterClass (groups = {"google","wikipedia","youtube"})
     public void closeTest(){
 
         long id = Thread.currentThread().getId();
@@ -86,4 +180,5 @@ public class testParallel {
         //myWebDriver.quit();
 
     }
+
 }
